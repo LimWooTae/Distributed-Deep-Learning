017b9442215bb06e3cca4a7a8890fc1037ea92e6
Limwootae <ahahzn1@naver.com>
2020-07-22 17:51:21 +0900

    sync1,2

---
+++ mnist/Model.py
@@ -0,0 +1,109 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import numpy as np
+
+class Model:
+    def __init__(self):
+        # Data: mnist dataset
+        self.data = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+        # CNN model
+        self.x = tf.placeholder(tf.float32, [None, 784])
+        self.x_image = tf.reshape(self.x, [-1,28,28,1])
+        self.y_ = tf.placeholder(tf.float32, [None, 10])
+        '''First Conv layer'''
+        # shape: [5,5,1,32]
+        # tf.truncated_normal(shape, mean=0.0, stddev=1.0, dtype=tf.float32, seed=None, name=None)
+        self.w_conv1 = tf.Variable(tf.truncated_normal([5,5,1,32], stddev=0.1))
+        # shape: [32]
+        self.b_conv1 = tf.Variable(tf.constant(0.1, shape=[32]))
+        # conv layer
+        self.conv1 = tf.nn.conv2d(self.x_image, self.w_conv1, strides=[1,1,1,1], padding='SAME')
+        # activation layer
+        self.h_conv1 = tf.nn.relu(self.conv1 + self.b_conv1)
+        self.h_pool1 = tf.nn.max_pool(self.h_conv1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+        '''Second Conv layer'''
+        # shape: [5,5,32,64]
+        self.w_conv2 = tf.Variable(tf.truncated_normal([5,5,32,64], stddev=0.1))
+        # shape: [64]
+        self.b_conv2 = tf.Variable(tf.constant(0.1, shape=[64]))
+        # conv layer
+        self.conv2 = tf.nn.conv2d(self.h_pool1, self.w_conv2, strides=[1,1,1,1], padding='SAME')
+        # activation layer
+        self.h_conv2 = tf.nn.relu(self.conv2 + self.b_conv2)
+        self.h_pool2 = tf.nn.max_pool(self.h_conv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+        '''FC layer'''
+        self.w_fc1 = tf.Variable(tf.truncated_normal([7*7*64, 1024], stddev=0.1))
+        self.b_fc1 = tf.Variable(tf.constant(0.1, shape=[1024]))
+        self.h_pool2_flat = tf.reshape(self.h_pool2, [-1, 7*7*64])
+        self.h_fc1 = tf.nn.relu(tf.matmul(self.h_pool2_flat, self.w_fc1) + self.b_fc1)
+
+        '''Dropout'''
+        self.keep_prob = tf.placeholder(tf.float32)
+        self.h_fc1_drop = tf.nn.dropout(self.h_fc1, self.keep_prob)
+
+        '''Softmax layer'''
+        self.w_fc2 = tf.Variable(tf.truncated_normal([1024, 10], stddev=0.1))
+        self.b_fc2 = tf.Variable(tf.constant(0.1, shape=[10]))
+        self.y = tf.nn.softmax(tf.matmul(self.h_fc1_drop, self.w_fc2) + self.b_fc2)
+
+        '''Cost function & optimizer'''
+        self.cost = tf.reduce_mean(-tf.reduce_sum(self.y_ * tf.log(self.y), reduction_indices=[1]))
+        #tf.train.AdamOptimizer.__init__(learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-08, use_locking=False, name='Adam')
+        self.optimizer = tf.train.AdamOptimizer(1e-4)
+        #tf.train.Optimizer.minimize(loss, global_step=None, var_list=None, gate_gradients=1, aggregation_method=None, colocate_gradients_with_ops=False, name=None, grad_loss=None)
+        self.train_step = self.optimizer.minimize(self.cost)
+
+        # Gradients
+        #tf.train.Optimizer.compute_gradients(loss, var_list=None, gate_gradients=1, aggregation_method=None, colocate_gradients_with_ops=False, grad_loss=None)
+        self.grads = self.optimizer.compute_gradients(self.cost,
+                        [self.w_conv1, self.b_conv1,
+                        self.w_conv2, self.b_conv2,
+                        self.w_fc1, self.b_fc1,
+                        self.w_fc2, self.b_fc2])
+        # Data1
+        self.d1_conv1_gw = np.empty((5,5,1,32), dtype=np.float32)
+        self.d1_conv1_gb = np.empty(32, dtype=np.float32)
+        self.d1_conv2_gw = np.empty((5,5,32,64), dtype=np.float32)
+        self.d1_conv2_gb = np.empty(64, dtype=np.float32)
+        self.d1_fc1_gw = np.empty((7*7*64,1024), dtype=np.float32)
+        self.d1_fc1_gb = np.empty(1024, dtype=np.float32)
+        self.d1_fc2_gw = np.empty((1024,10), dtype=np.float32)
+        self.d1_fc2_gb = np.empty(10, dtype=np.float32)
+        # Data2
+        self.d2_conv1_gw = np.empty((5,5,1,32), dtype=np.float32)
+        self.d2_conv1_gb = np.empty(32, dtype=np.float32)
+        self.d2_conv2_gw = np.empty((5,5,32,64), dtype=np.float32)
+        self.d2_conv2_gb = np.empty(64, dtype=np.float32)
+        self.d2_fc1_gw = np.empty((7*7*64,1024), dtype=np.float32)
+        self.d2_fc1_gb = np.empty(1024, dtype=np.float32)
+        self.d2_fc2_gw = np.empty((1024,10), dtype=np.float32)
+        self.d2_fc2_gb = np.empty(10, dtype=np.float32)
+        # Broadcasting values
+        self.bcast_conv1_w = np.empty((5,5,1,32), dtype=np.float32)
+        self.bcast_conv1_b = np.empty(32, dtype=np.float32)
+        self.bcast_conv2_w = np.empty((5,5,32,64), dtype=np.float32)
+        self.bcast_conv2_b = np.empty(64, dtype=np.float32)
+        self.bcast_fc1_w = np.empty((7*7*64,1024), dtype=np.float32)
+        self.bcast_fc1_b = np.empty(1024, dtype=np.float32)
+        self.bcast_fc2_w = np.empty((1024,10), dtype=np.float32)
+        self.bcast_fc2_b = np.empty(10, dtype=np.float32)
+
+        # For evaluating
+        self.prediction = tf.equal(tf.argmax(self.y,1), tf.argmax(self.y_, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(self.prediction, tf.float32))
+        self.test_x = self.data.test.images
+        self.test_y_ = self.data.test.labels
+
+        # Create session
+        self.sess = tf.Session()
+
+        # Initialize variables
+        self.sess.run(tf.global_variables_initializer())
+
+
+    def train(self):
+        # Train specific model here
+        raise NotImplementedError("Train method not implemented")
---
+++ sync1.py
@@ -0,0 +1,215 @@
+from typing import List
+from mnist import Model
+#import Model
+from mpi4py import MPI
+import numpy as np
+import tensorflow as tf
+import time
+
+class SyncWorker(Model):
+    def __init__(self, comm, rank, batch_size):
+        super().__init__()
+        
+        self.comm = comm
+
+        # Rank 0: parameter server
+        # Rank 1,2: worker
+        self.rank = rank
+        self.batch_size = batch_size
+
+
+    def work(self) -> List:
+        x_batch, y_batch = self.data.train.next_batch(self.batch_size)
+        # Compute gradient values
+        ret, = self.sess.run([self.grads], feed_dict={self.x: x_batch, self.y_: y_batch, self.keep_prob: 0.5})
+        
+        # ret -> ((gw, w), (gb, b))
+        grads = [grad for grad, var in ret]
+
+        # Tuple: (gradient, variable)
+        # Pack gradeint values
+        # Data: [gw_conv1, gb_conv1, gw_conv2, gb_conv2, gw_fc1, gb_fc1, gw_fc2, gb_fc2]
+        # Send data to parameter server
+        return grads
+
+
+    def update(self, vars):
+        
+        w_conv1 = tf.compat.v1.assign(self.w_conv1, vars[0])
+        b_conv1 = tf.compat.v1.assign(self.b_conv1, vars[1])
+        w_conv2 = tf.compat.v1.assign(self.w_conv2, vars[2])
+        b_conv2 = tf.compat.v1.assign(self.b_conv2, vars[3])
+        w_fc1 = tf.compat.v1.assign(self.w_fc1, vars[4])
+        b_fc1 = tf.compat.v1.assign(self.b_fc1, vars[5])
+        w_fc2 = tf.compat.v1.assign(self.w_fc2, vars[6])
+        b_fc2 = tf.compat.v1.assign(self.b_fc2, vars[7])
+        '''
+        w_conv1 = tf.assign(self.w_conv1, vars[0])
+        b_conv1 = tf.assign(self.b_conv1, vars[1])
+        w_conv2 = tf.assign(self.w_conv2, vars[2])
+        b_conv2 = tf.assign(self.b_conv2, vars[3])
+        w_fc1 = tf.assign(self.w_fc1, vars[4])
+        b_fc1 = tf.assign(self.b_fc1, vars[5])
+        w_fc2 = tf.assign(self.w_fc2, vars[6])
+        b_fc2 = tf.assign(self.b_fc2, vars[7])
+        '''
+        self.sess.run([
+            w_conv1, b_conv1,
+            w_conv2, b_conv2,
+            w_fc1, b_fc1,
+            w_fc2, b_fc2
+        ])
+
+
+class ParameterServer(Model):
+    def __init__(self, comm, rank):
+        self.comm = comm
+        super().__init__()
+
+        # Rank 0: parameter server
+        # Rank 1,2: worker
+        self.rank = rank
+
+
+    def sync(self) -> List:
+        # Receive data from workers
+        # From worker1
+        comm.Recv([self.d1_conv1_gw, MPI.DOUBLE], source=1, tag=1)
+        comm.Recv([self.d1_conv1_gb, MPI.DOUBLE], source=1, tag=2)
+        comm.Recv([self.d1_conv2_gw, MPI.DOUBLE], source=1, tag=3)
+        comm.Recv([self.d1_conv2_gb, MPI.DOUBLE], source=1, tag=4)
+        comm.Recv([self.d1_fc1_gw, MPI.DOUBLE], source=1, tag=5)
+        comm.Recv([self.d1_fc1_gb, MPI.DOUBLE], source=1, tag=6)
+        comm.Recv([self.d1_fc2_gw, MPI.DOUBLE], source=1, tag=7)
+        comm.Recv([self.d1_fc2_gb, MPI.DOUBLE], source=1, tag=8)
+
+        # From worker2
+        comm.Recv([self.d2_conv1_gw, MPI.DOUBLE], source=2, tag=1)
+        comm.Recv([self.d2_conv1_gb, MPI.DOUBLE], source=2, tag=2)
+        comm.Recv([self.d2_conv2_gw, MPI.DOUBLE], source=2, tag=3)
+        comm.Recv([self.d2_conv2_gb, MPI.DOUBLE], source=2, tag=4)
+        comm.Recv([self.d2_fc1_gw, MPI.DOUBLE], source=2, tag=5)
+        comm.Recv([self.d2_fc1_gb, MPI.DOUBLE], source=2, tag=6)
+        comm.Recv([self.d2_fc2_gw, MPI.DOUBLE], source=2, tag=7)
+        comm.Recv([self.d2_fc2_gb, MPI.DOUBLE], source=2, tag=8)
+
+        # Apply gradients
+        conv1_gw = self.d1_conv1_gw + self.d2_conv1_gw
+        conv1_gb = self.d1_conv1_gb + self.d2_conv1_gb
+        conv2_gw = self.d1_conv2_gw + self.d2_conv2_gw
+        conv2_gb = self.d1_conv2_gb + self.d2_conv2_gb
+        fc1_gw = self.d1_fc1_gw + self.d2_fc1_gw
+        fc1_gb = self.d1_fc1_gb + self.d2_fc1_gb
+        fc2_gw = self.d1_fc2_gw + self.d2_fc2_gw
+        fc2_gb = self.d1_fc2_gb + self.d2_fc2_gb
+        apply_gradients = self.optimizer.apply_gradients((
+            (conv1_gw, self.w_conv1), (conv1_gb, self.b_conv1),
+            (conv2_gw, self.w_conv2), (conv2_gb, self.b_conv2),
+            (fc1_gw, self.w_fc1), (fc1_gb, self.b_fc1),
+            (fc2_gw, self.w_fc2), (fc2_gb, self.b_fc2),
+        ))
+        self.sess.run(apply_gradients)
+
+        # Broadcast variables
+        self.bcast_conv1_w = self.w_conv1.eval(session=self.sess)
+        self.bcast_conv1_b = self.b_conv1.eval(session=self.sess)
+        self.bcast_conv2_w = self.w_conv2.eval(session=self.sess)
+        self.bcast_conv2_b = self.b_conv2.eval(session=self.sess)
+        self.bcast_fc1_w = self.w_fc1.eval(session=self.sess)
+        self.bcast_fc1_b = self.b_fc1.eval(session=self.sess)
+        self.bcast_fc2_w = self.w_fc2.eval(session=self.sess)
+        self.bcast_fc2_b = self.b_fc2.eval(session=self.sess)
+
+        return [
+            self.bcast_conv1_w, self.bcast_conv1_b, self.bcast_conv2_w, self.bcast_conv2_b,
+            self.bcast_fc1_w, self.bcast_fc1_b, self.bcast_fc2_w, self.bcast_fc2_b
+        ]
+        
+
+if __name__ == "__main__":
+    training_time = 1
+    batch_size = 100
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    ps = ParameterServer(comm, rank)
+    w1 = SyncWorker(comm, rank, batch_size)
+    w2 = SyncWorker(comm, rank, batch_size)
+    
+    p = Model()
+    
+    '''
+    bcast_conv1_w = 0
+    bcast_conv1_b = 0
+    bcast_conv2_w = 0
+    bcast_conv2_b = 0
+    bcast_fc1_w = 0
+    bcast_fc1_b = 0
+    bcast_fc2_w = 0
+    bcast_fc2_b = 0
+    '''
+
+    '''
+    # Mapping variables
+    vars = [
+        bcast_conv1_w, bcast_conv1_b, bcast_conv2_w, bcast_conv2_b,
+        bcast_fc1_w, bcast_fc1_b, bcast_fc2_w, bcast_fc2_b
+    ]
+    '''
+    vars = [
+        p.bcast_conv1_w, p.bcast_conv1_b, p.bcast_conv2_w, p.bcast_conv2_b,
+        p.bcast_fc1_w, p.bcast_fc1_b, p.bcast_fc2_w, p.bcast_fc2_b
+    ]
+    
+    # Measure time
+    if rank == 0:
+        start = time.clock()
+
+    for step in range(training_time):
+        print('training_time : ', step)
+        # Parameter Server
+        if rank == 0:
+            vars = ps.sync()
+
+        # Worker1
+        elif rank == 1:
+            grads_w1 = w1.work()
+
+            # Send worker 1's grads
+            for i in range(8):
+                comm.Send([grads_w1[i], MPI.DOUBLE], dest=0, tag=i+1)
+        
+        # Worker2
+        else:
+            grads_w2 = w2.work()
+
+            # Send worker 2's grads
+            for i in range(8):
+                comm.Send([grads_w2[i], MPI.DOUBLE], dest=0, tag=i+1)
+
+
+        # Receive data from parameter server
+        comm.Bcast([vars[0], MPI.DOUBLE], root=0)
+        comm.Bcast([vars[1], MPI.DOUBLE], root=0)
+        comm.Bcast([vars[2], MPI.DOUBLE], root=0)
+        comm.Bcast([vars[3], MPI.DOUBLE], root=0)
+        comm.Bcast([vars[4], MPI.DOUBLE], root=0)
+        comm.Bcast([vars[5], MPI.DOUBLE], root=0)
+        comm.Bcast([vars[6], MPI.DOUBLE], root=0)
+        comm.Bcast([vars[7], MPI.DOUBLE], root=0)
+        
+        # Update variables (worker)
+        if rank == 1:
+            w1.update(vars)
+
+        elif rank == 2:
+            w2.update(vars)
+            
+        
+    
+    # Evaluate model
+    if rank == 0:#if self.rank == 0:
+        end = time.clock()
+        
+        print(p.sess.run(p.accuracy, feed_dict={p.x: p.test_x, p.y_: p.test_y_, p.keep_prob: 1.0}))
+        #print(self.sess.run(self.accuracy, feed_dict={self.x: self.test_x, self.y_: self.test_y_, self.keep_prob: 1.0}))
+        print(end-start)
