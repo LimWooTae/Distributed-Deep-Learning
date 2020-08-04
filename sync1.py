c7acd040c31433fa7e588aa8b30363df50390d2c
Limwootae <ahahzn1@naver.com>
2020-08-04 22:48:27 +0900

    use iteration

--- sync1.py
+++ sync1.py
@@ -3,8 +3,9 @@ from mnist import Model
 #import Model
 from mpi4py import MPI
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import time
+tf.disable_v2_behavior()
 
 class SyncWorker(Model):
     def __init__(self, comm, rank, batch_size):
@@ -12,24 +13,14 @@ class SyncWorker(Model):
         self.comm = comm
         self.rank = rank
         self.batch_size = batch_size
-        self.app = self.optimizer.apply_gradients(self.grads)
-        
-        self.temp0 = tf.zeros(shape = self.w_conv1.shape)
-        self.temp1 = tf.zeros(shape = self.b_conv1.shape)
-        self.temp2 = tf.zeros(shape = self.w_conv2.shape)
-        self.temp3 = tf.zeros(shape = self.b_conv2.shape)
-        self.temp4 = tf.zeros(shape = self.w_fc1.shape)
-        self.temp5 = tf.zeros(shape = self.b_fc1.shape)
-        self.temp6 = tf.zeros(shape = self.w_fc2.shape)
-        self.temp7 = tf.zeros(shape = self.b_fc2.shape)
+        self.temp = [np.empty(self.var_shape[i], dtype=np.float32) for i in range(self.var_size)]
 
     def work(self) -> List:
         x_batch, y_batch = self.data.train.next_batch(self.batch_size)
         # Compute gradient values
         ret, = self.sess.run([self.grads], feed_dict={self.x: x_batch, self.y_: y_batch, self.keep_prob: 0.5})
-
-        # ret -> ((gw, w), (gb, b))
         
+        # ret -> ((gw, w), (gb, b))
         grads = [grad for grad, var in ret]
         self.sess.run(self.grads_and_vars, feed_dict={self.x: x_batch, self.y_: y_batch, self.keep_prob: 0.5})
         
@@ -39,16 +30,9 @@ class SyncWorker(Model):
         # Send data to parameter server
         return grads
         
-    def apply(self): # 값은 바뀐다.
-        
-        self.w_conv1 = self.temp0
-        self.b_conv1 = self.temp1
-        self.w_conv2 = self.temp2
-        self.b_conv2 = self.temp3
-        self.w_fc1 = self.temp4
-        self.b_fc1 = self.temp5
-        self.w_fc2 = self.temp6
-        self.b_fc2 = self.temp7
+    def apply(self):
+        for i in range(8):
+            self.layer[i] = self.temp[i]
             
 class ParameterServer(Model):
     def __init__(self, comm, rank):
@@ -58,43 +42,20 @@ class ParameterServer(Model):
         
     def sync(self) -> List:
         # Receive data from workers
-        # From worker1
-        comm.Recv([self.d1_conv1_gw, MPI.DOUBLE], source=1, tag=1)
-        comm.Recv([self.d1_conv1_gb, MPI.DOUBLE], source=1, tag=2)
-        comm.Recv([self.d1_conv2_gw, MPI.DOUBLE], source=1, tag=3)
-        comm.Recv([self.d1_conv2_gb, MPI.DOUBLE], source=1, tag=4)
-        comm.Recv([self.d1_fc1_gw, MPI.DOUBLE], source=1, tag=5)
-        comm.Recv([self.d1_fc1_gb, MPI.DOUBLE], source=1, tag=6)
-        comm.Recv([self.d1_fc2_gw, MPI.DOUBLE], source=1, tag=7)
-        comm.Recv([self.d1_fc2_gb, MPI.DOUBLE], source=1, tag=8)
+        for i in range(8):
+            comm.Recv([self.d1[i], MPI.DOUBLE], source=1, tag=i+1)
+        
+        for i in range(8):
+            comm.Recv([self.d2[i], MPI.DOUBLE], source=2, tag=i+1)
+        
+        for i in range(8):
+            self.layer[i] = (self.d1[i] + self.d2[i])/2
 
-        # From worker2
-        comm.Recv([self.d2_conv1_gw, MPI.DOUBLE], source=2, tag=1)
-        comm.Recv([self.d2_conv1_gb, MPI.DOUBLE], source=2, tag=2)
-        comm.Recv([self.d2_conv2_gw, MPI.DOUBLE], source=2, tag=3)
-        comm.Recv([self.d2_conv2_gb, MPI.DOUBLE], source=2, tag=4)
-        comm.Recv([self.d2_fc1_gw, MPI.DOUBLE], source=2, tag=5)
-        comm.Recv([self.d2_fc1_gb, MPI.DOUBLE], source=2, tag=6)
-        comm.Recv([self.d2_fc2_gw, MPI.DOUBLE], source=2, tag=7)
-        comm.Recv([self.d2_fc2_gb, MPI.DOUBLE], source=2, tag=8)
-        
-        self.conv1_gw = (self.d1_conv1_gw + self.d2_conv1_gw)/2
-        self.conv1_gb = (self.d1_conv1_gb + self.d2_conv1_gb)/2
-        self.conv2_gw = (self.d1_conv2_gw + self.d2_conv2_gw)/2
-        self.conv2_gb = (self.d1_conv2_gb + self.d2_conv2_gb)/2
-        self.fc1_gw = (self.d1_fc1_gw + self.d2_fc1_gw)/2
-        self.fc1_gb = (self.d1_fc1_gb + self.d2_fc1_gb)/2
-        self.fc2_gw = (self.d1_fc2_gw + self.d2_fc2_gw)/2
-        self.fc2_gb = (self.d1_fc2_gb + self.d2_fc2_gb)/2
-        
-        return [
-            self.conv1_gw, self.conv1_gb, self.conv2_gw, self.conv2_gb,
-            self.fc1_gw, self.fc1_gb, self.fc2_gw, self.fc2_gb
-        ]
+        return self.layer
 
 if __name__ == "__main__":
-    training_time = 200
-    batch_size = 100
+    training_time = 100
+    batch_size = 128
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     if rank == 0:
@@ -105,20 +66,7 @@ if __name__ == "__main__":
         w2 = SyncWorker(comm, rank, batch_size)
     
     p = Model()
-    conv1_gw = np.empty(p.w_conv1.shape, dtype=np.float32)
-    conv1_gb = np.empty(p.b_conv1.shape, dtype=np.float32)
-    conv2_gw = np.empty(p.w_conv2.shape, dtype=np.float32)
-    conv2_gb = np.empty(p.b_conv2.shape, dtype=np.float32)
-    fc1_gw = np.empty(p.w_fc1.shape, dtype=np.float32)
-    fc1_gb = np.empty(p.b_fc1.shape, dtype=np.float32)
-    fc2_gw = np.empty(p.w_fc2.shape, dtype=np.float32)
-    fc2_gb = np.empty(p.b_fc2.shape, dtype=np.float32)
-    
-    # Mapping variables
-    vars = [
-        conv1_gw, conv1_gb, conv2_gw, conv2_gb,
-        fc1_gw, fc1_gb, fc2_gw, fc2_gb
-    ]
+    vars = [np.empty(p.var_shape[i], dtype=np.float32) for i in range(p.var_size)]
     
     if rank == 0:
         start = time.time()
@@ -136,7 +84,6 @@ if __name__ == "__main__":
         # Worker1
         elif rank == 1:
             grads_w1 = w1.work()
-            temp = w1.w_conv1
             # Send worker 1's grads
             for i in range(8):
                 comm.Send([grads_w1[i], MPI.DOUBLE], dest=0, tag=i+1)
